69a2ad739abb55c890702d8e538733c406634e83
decagon <udemekendrick@gmail.com>
2023-08-16 17:07:06 +0100

    Post request added.

---
+++ src/main/java/com/CB/microservices/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.CB.microservices.service;
+
+import com.CB.microservices.model.Employee;
+
+public interface EmployeeService {
+
+    Employee save(Employee employee);
+}
---
+++ src/main/java/com/CB/microservices/service/EmployeeServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.CB.microservices.service;
+
+import com.CB.microservices.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService{
+
+    List<Employee> employees = new ArrayList<>();
+
+    @Override
+    public Employee save(Employee employee) {
+
+        if (employee.getEmployeeId() == null || employee.getEmployeeId().isEmpty()){
+            employee.setEmployeeId(UUID.randomUUID().toString());
+        }
+        employees.add(employee);
+        return employee;
+    }
+}
